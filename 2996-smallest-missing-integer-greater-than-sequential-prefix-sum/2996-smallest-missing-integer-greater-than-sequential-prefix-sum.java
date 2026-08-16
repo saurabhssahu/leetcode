23839053f9e7d92c2a1c0d2036b442f39af18e54
Saurabh Sahu <77149317+saurabhssahu@users.noreply.github.com>
2026-08-16 18:33:15 +0530

    Time: 2 ms (52.17%), Space: 44 MB (58.51%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -1,33 +1,26 @@
-import java.util.*;
-
 class Solution {
-    public int missingInteger(int[] nums) {
-        
-        int result = nums[0];
 
-        for(int i = 0; i < nums.length - 1; i++) {
-            if (nums[i+1] - nums[i] == 1) {
-                result += nums[i+1];
+    public int missingInteger(int[] nums) {
+        int n = nums.length;
+        Set<Integer> numSet = new HashSet<>(n);
+        for (int num : nums) {
+            numSet.add(num);
+        }
+        int prefixLen = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                prefixLen += 1;
             } else {
                 break;
             }
         }
 
-
-        boolean found = true;
-
-        while (found) {
-            found = false;
-            for (int x : nums) {
-                if (x == result) {
-                    result++;    // Safely incremented
-                    found = true;// Triggers the loop to check the array again
-                    break;       // Optimization: stop checking the rest of the array
-                }
-            }
+        int total = ((nums[prefixLen - 1] + nums[0]) * prefixLen) / 2;
+        while (numSet.contains(total)) {
+            total += 1;
         }
 
-        return result;
-        
+        return total;
     }
 }
